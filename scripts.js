191647b77fb3e4337693561d5133223e129f621b
Mariyam Shaheedha <mariyam@LP-787.stoho.local>
2023-06-10 05:32:11 +0500

    comments

--- scripts.js
+++ scripts.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
     selectRandomWord();
 });
 
+//randomly selects a word as traget word and is stored in word variable.
 selectRandomWord = () => {
     word = validWords[Math.floor(Math.random() * validWords.length)]
     console.log(word);
 }
+//renders the view to show user input
 handleInput = () => {
     let inputBoxes = document.querySelectorAll('.active_row .letter');
     for (let index = 0; index != inputBoxes.length; index++) {
@@ -24,11 +26,13 @@ handleInput = () => {
     }
 }
 
+//removes letters from guessed letter
 backspaceLetter = () => {
     guessLetters.pop();
     handleInput();
 }
 
+//handles all the keydowns, handles for letters, backspace and enter
 document.addEventListener('keydown', function(event) {
     if (event.key.match(/^[a-z]$/i)) {
         if(guessLetters.length != 5){
@@ -42,7 +46,8 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-for (var i = 0; i < keys.length; i++) {
+//adds an event listner to the keyboard in the view
+for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', function(){
         if(guessLetters.length != 5){
             guessLetters.push(this.textContent.toUpperCase());
@@ -57,9 +62,10 @@ document.getElementById("btnEnter").addEventListener('click', function(){
 
 document.getElementById("btnBackspace").addEventListener('click', backspaceLetter);
 
+//this function evaluates the guess letter
 evaluateInputWord = () => {
     let inputBoxes = document.querySelectorAll('.active_row .letter');
-    
+    //here i am giving a validation to allow only 5 letter words.
     if(guessLetters.length != 5){
         document.getElementById("error-title").innerText = "Not enough Letters!";
         document.getElementById("error-message").innerText = "It should be a 5 letter word. Please try again."
@@ -68,6 +74,7 @@ evaluateInputWord = () => {
             document.getElementById("errorModal").style.display = 'none';
         }, 3000);
     }else{
+        //
         if(guessLetters.join("") === word){
             document.getElementById("wonModal").style.display = 'block';
         }else if(!validWords.includes(guessLetters.join(""))){
@@ -93,6 +100,7 @@ evaluateInputWord = () => {
     guessLetters = [];
 }
 
+//a function to analyse letters individually
 evaluateLetter = (inputBoxes) => {
     for (let i = 0; i < word.length; i++) {
         if(inputBoxes[i].value == word[i]){
@@ -103,6 +111,7 @@ evaluateLetter = (inputBoxes) => {
     }    
 }
 
+//just a function to change the focused row and indication the turn
 nextRow = () => {
     let activeRow = document.querySelector('.active_row');
     activeRow.classList.remove('active_row');
@@ -110,6 +119,7 @@ nextRow = () => {
     sibling.classList.add('active_row');
 }
 
+//a function to clear all and start the game fresh
 newRound = () => {
     turn = 0;
     guessLetters = [];
